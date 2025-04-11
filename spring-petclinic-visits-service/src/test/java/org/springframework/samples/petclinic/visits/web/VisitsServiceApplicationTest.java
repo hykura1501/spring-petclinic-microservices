166581f6efd47d175cea07c6aaa6141696203d57
PhanPhuc269 <164202651+PhanPhuc269@users.noreply.github.com>
2025-04-11 17:06:52 +0700

    Add testcase to spring-petclinic-visits-service

--- spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitsServiceApplicationTest.java
+++ spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitsServiceApplicationTest.java
@@ -5,10 +5,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.petclinic.visits.VisitsServiceApplication;
 
 @SpringBootTest(classes = VisitsServiceApplication.class)
-public class VisitsServiceApplicationTest {
+class VisitsServiceApplicationTest {
 
     @Test
-    public void main() {
-        VisitsServiceApplication.main(new String[]{});
+    void contextLoads() {
+        // Test that the Spring application context loads successfully
     }
 }
